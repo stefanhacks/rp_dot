4be0a16da02aed9618fa0814a65547497dc3e4cf
Filipe Stefano <filipe.ghisi@gmail.com>
2025-02-08 16:28:06 -0300

    Added keep high and keep low for tests.

--- scenes/tests/test_dice_statistic.gd
+++ scenes/tests/test_dice_statistic.gd
@@ -5,25 +5,36 @@ extends Control
 
 var number_of_rolls = 0.0
 var sum_of_rolls = 0.0
+var sum_of_rolls_keep_high = 0.0
+var sum_of_rolls_keep_low = 0.0
 var max_bar_size = 260.0
 
-@onready var texture_rect: TextureRect = $TextureRect
+@onready var bar_1: TextureRect = $Bar1
+@onready var bar_2: TextureRect = $Bar2
+@onready var bar_3: TextureRect = $Bar3
+
 @onready var dice_label: Label = $DiceLabel
 @onready var avg_label: Label = $AvgLabel
 
 
 func _ready() -> void:
 	dice_label.text = "D%s" % dice_type
-	texture_rect.modulate = bar_color
+	modulate = bar_color
 
 
-func _process(_delta: float) -> void:
-	number_of_rolls += 1
-	sum_of_rolls += DiceTray.roll(dice_type)
+func _physics_process(delta: float) -> void:
 	_update_bar()
 
 
 func _update_bar() -> void:
+	number_of_rolls += 1
+	sum_of_rolls += DiceTray.roll(dice_type)
+	sum_of_rolls_keep_low += DiceTray.roll_two_keep_low(dice_type)
+	sum_of_rolls_keep_high += DiceTray.roll_two_keep_high(dice_type)
+	
 	var average = sum_of_rolls / number_of_rolls
-	texture_rect.size.y = max_bar_size * average / dice_type
 	avg_label.text = "%2.2f" % average
+	
+	bar_1.size.y = max_bar_size * (sum_of_rolls_keep_low / number_of_rolls) / dice_type
+	bar_2.size.y = max_bar_size * average / dice_type
+	bar_3.size.y = max_bar_size * (sum_of_rolls_keep_high / number_of_rolls) / dice_type
