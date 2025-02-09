c39948509102df1fa7b8b687c1594a02ef380e4d
Filipe Stefano <filipe.ghisi@gmail.com>
2025-02-08 23:01:43 -0300

    Added a way to compare pure RNG with DiceRoll packet class. Tests seem ok.

--- scenes/tests/test_dice_statistic.gd
+++ scenes/tests/test_dice_statistic.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@export var pure_test = false
 @export var bar_color := Color.WHITE
 @export var dice_type: DiceTray.Dice = DiceTray.Dice.D4
 
@@ -29,9 +30,15 @@ func _physics_process(_delta: float) -> void:
 
 func _update_bar() -> void:
 	number_of_rolls += 1.0
-	sum_of_rolls += DiceTray.roll(dice_type).sum
-	sum_of_rolls_keep_low += DiceTray.roll(dice_type, 0, DiceTray.RollStyle.TWICE_KEEP_LOW).sum
-	sum_of_rolls_keep_high += DiceTray.roll(dice_type, 0, DiceTray.RollStyle.TWICE_KEEP_HIGH).sum
+	
+	if pure_test == true:
+		sum_of_rolls += randi_range(1, dice_type)
+		sum_of_rolls_keep_low += mini(randi_range(1, dice_type), randi_range(1, dice_type))
+		sum_of_rolls_keep_high += maxi(randi_range(1, dice_type), randi_range(1, dice_type))
+	else:
+		sum_of_rolls += DiceTray.roll(dice_type).sum
+		sum_of_rolls_keep_low += DiceTray.roll(dice_type, 0, DiceTray.RollStyle.TWICE_KEEP_LOW).sum
+		sum_of_rolls_keep_high += DiceTray.roll(dice_type, 0, DiceTray.RollStyle.TWICE_KEEP_HIGH).sum
 	
 	var average = float(sum_of_rolls) / number_of_rolls
 	avg_label.text = "%2.2f" % average
