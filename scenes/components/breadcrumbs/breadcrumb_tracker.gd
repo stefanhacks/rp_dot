44e113e41d945608794f1516a1860780cb276adb
Filipe Stefano <filipe.ghisi@gmail.com>
2025-02-15 18:41:04 -0300

    Added pooling to Breadcrumb Tracker.

--- scenes/components/breadcrumbs/breadcrumb_tracker.gd
+++ scenes/components/breadcrumbs/breadcrumb_tracker.gd
@@ -2,33 +2,62 @@ class_name BreadcrumbTracker
 extends Node
 
 const BREADCRUMB = preload("res://scenes/components/breadcrumbs/breadcrumb.tscn")
+const BREADCRUMB_AMOUNT = 10
 
 @export var a_star_manager: AStarManager
 @export var walk_color: Color = Color.MEDIUM_SPRING_GREEN
 @export var run_color: Color = Color.CORAL
 @export var x_color: Color = Color.REBECCA_PURPLE
 
+var breadcrumb_pool: Array[Node2D]
+var used_nodes: Array[Node2D]
 
-func remove_first() -> void:
-	var first = get_children().pop_front()
-	first.free()
+
+func _ready() -> void:
+	for i in range(BREADCRUMB_AMOUNT):
+		var node = _make_breadcrumb_node()
+		breadcrumb_pool.push_back(node)
+
+
+func _make_breadcrumb_node() -> Node2D:
+	var node = BREADCRUMB.instantiate()
+	node.visible = false
+	add_child(node)
+	return node
+
+
+func get_breadcrumb_node() -> Node2D:
+	var node = breadcrumb_pool.pop_front()
+	if node == null: node = _make_breadcrumb_node()
+	
+	used_nodes.push_back(node)
+	node.visible = true
+	return node
+
+
+func _clean_breadcrumb(node: Node) -> void:
+	node.visible = false
+	breadcrumb_pool.push_back(node)
 
 
 func clean_breadcrumbs() -> void:
-	for breadcrumb in get_children():
-		breadcrumb.free()
+	for i in range(used_nodes.size()):
+		remove_first()
+
+
+func remove_first() -> void:
+	var node = used_nodes.pop_front()
+	_clean_breadcrumb(node)
 
 
 func place_breadcrumb(at_position: Vector2i, indication: Breadcrumb.Indication) -> void:
-	var breadcrumb = BREADCRUMB.instantiate()
+	var breadcrumb = get_breadcrumb_node()
 	breadcrumb.position = at_position
 	breadcrumb.kind = indication
 	
 	var is_walk = indication == Breadcrumb.Indication.WALK or indication == Breadcrumb.Indication.WALK_END
 	var is_run = indication == Breadcrumb.Indication.RUN or indication == Breadcrumb.Indication.RUN_END
 	breadcrumb.modulate = walk_color if is_walk else run_color if is_run else x_color
-	
-	add_child(breadcrumb)
 
 
 func place_breadcrumbs_on_grid(cell_positions: Array, walk_limit: int, run_limit: int) -> void:
